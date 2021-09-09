296721b8dfc9c40fd2e4dfba327d3d92fc73712a
Raphael Lima <raphaeldesousalm@gmail.com>
2021-09-09 18:23:27 -0300

    Projeto Final

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+// initial data
+
+const calculadora = criaCalculadora()
+
+//events
+
+calculadora.inicia()
+
+//functions
+
+function criaCalculadora() {
+  return {
+    //Atributos do obj
+
+    display: document.querySelector('.display'),
+    clear: document.querySelector('.btn-clear'),
+
+    //metodos do obj
+    inicia: function () {
+      this.cliqueiBotoes()
+      this.pressEnter()
+    },
+
+    pressEnter: function () {
+      document.addEventListener('keypress', e => {
+        if (e.code === 'Enter') {
+          this.realizaConta()
+        }
+        return
+      })
+    },
+
+    cliqueiBotoes: function () {
+      document.addEventListener('click', e => {
+        const el = e.target
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText)
+          this.display.focus()
+        } else if (el.classList.contains('btn-clear')) {
+          this.clearDisplay()
+          this.display.focus()
+        } else if (el.classList.contains('btn-del')) {
+          this.deleteOne()
+          this.display.focus()
+        } else if (el.classList.contains('btn-eq')) {
+          this.realizaConta()
+          this.display.focus()
+        }
+      })
+    },
+
+    btnParaDisplay: function (valor) {
+      this.display.value += valor
+    },
+
+    clearDisplay: function () {
+      this.display.value = ''
+      this.display.focus()
+    },
+
+    deleteOne: function () {
+      this.display.value = this.display.value.slice(0, -1)
+    },
+
+    realizaConta: function () {
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+
+        if (!conta) {
+          alert('Conta inválida')
+        }
+
+        this.display.value = conta
+
+        this.display.focus()
+      } catch (e) {
+        alert('Conta inválida')
+        return
+      }
+    }
+  }
+}
