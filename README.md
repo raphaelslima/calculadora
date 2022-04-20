0fe2100ae5f883f910ace1575e58317001ff738a
Raphael Lima <raphaeldesousalm@gmail.com>
2022-04-19 21:52:50 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # calculadora
-Projeto que utiliza JavaScript para simular a calculadora do google.
-
-## Minha aplicação:
-
-<img src="./assets/screenshots/calc.PNG">
 
 ## Sobre a aplicação:
 
-Projeto que simula a maioria das funcionalidades da calculadora do google, capturando as teclas que o usuário clica 
-tanto no teclado como no layout da própria calculadora e realizando as operações matemáticas por meio do JavaScript.
-
-## Por que essa aplicão?
-
-Esse porjeto foi ensinado em um workshop da empresa b7web, e realizado com o intuito de desenvolver minha lógica de programação e o 
-portfólio pessoal.
+Nessa aplicação o usuário tem como primeiro passo inserir a quantidade de números que serão usados na operação de soma ou de subtrair, clicando nos números do layout e posteriormente confirmando, o próximo passo é inserir e confirma no botão de confirmar os valores que serão usados na operação, por fim basta apenas escolher se deseja somar ou subtrair.
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,75 +2,148 @@
 
 const calculadora = criaCalculadora()
 
-calculadora.inicia()
+calculadora.start()
 
 function criaCalculadora() {
   return {
+    //Propriedades iniciais da classe
     display: document.querySelector('.display'),
     clear: document.querySelector('.btn-clear'),
+    amountFields: 0,
+    numbers: [],
 
-    inicia: function () {
-      this.cliqueiBotoes()
-      this.pressEnter()
+    //Inicia a aplicação
+    start: function () {
+      this.clickBtn()
+      this.display.value = `Digite a quantidade de campos envolvidos na operação:`
     },
 
-    pressEnter: function () {
-      document.addEventListener('keypress', e => {
-        if (e.code === 'Enter') {
-          this.realizaConta()
-        }
-        return
-      })
-    },
-
-    cliqueiBotoes: function () {
+    //Controla o fluxo da aplicação de acordo com as interações do usuário
+    clickBtn: function () {
       document.addEventListener('click', e => {
         const el = e.target
         if (el.classList.contains('btn-num')) {
-          this.btnParaDisplay(el.innerText)
+          this.controllerDisplay()
+          this.btnValueDisplay(el.innerText)
           this.display.focus()
         } else if (el.classList.contains('btn-clear')) {
-          this.clearDisplay()
+          this.clear()
           this.display.focus()
+        } else if (el.classList.contains('btn-confirm')) {
+          if (this.amountFields === 0 && this.display.value !== '') {
+            if (this.display.value < 2) {
+              this.display.value = `Por favor, digite um número igual ao maior a 2`
+              return
+            }
+
+            this.amountFields = parseFloat(this.display.value)
+            this.display.value = `Por favor, digite os ${this.amountFields} números que serão utilizados na operação em sequência:`
+            return
+          } else if (this.amountFields > this.numbers.length) {
+            this.addNumbers(parseFloat(this.display.value))
+          }
+
+          if (this.amountFields === this.numbers.length) {
+            this.display.value = 'Deseja realizar soma ou subtração ?'
+          }
+        } else if (el.classList.contains('btn-plus')) {
+          if (this.numbers.length !== this.amountFields) {
+            this.display.value =
+              'Por favor, preencha e confirme todos os campos reservados para a operação'
+            return
+          }
+
+          let result = 0
+          if (this.amountFields !== 0) {
+            for (let i = 0; i < this.amountFields; i++) {
+              result = result + this.numbers[i]
+            }
+          }
+          this.display.value = ''
+          this.btnValueDisplay(`Resultado: ${result}`)
+          return
+        } else if (el.classList.contains('btn-less')) {
+          if (this.numbers.length !== this.amountFields) {
+            this.display.value =
+              'Por favor, preencha e confirme todos os campos reservados para a operação'
+            return
+          }
+
+          let result = this.numbers[0] ? this.numbers[0] : 0
+          if (this.amountFields !== 0) {
+            for (let i = 1; i < this.amountFields; i++) {
+              result = result - this.numbers[i]
+            }
+          }
+          this.display.value = ''
+          this.btnValueDisplay(`Resultado: ${result}`)
+          return
         } else if (el.classList.contains('btn-del')) {
           this.deleteOne()
-          this.display.focus()
-        } else if (el.classList.contains('btn-eq')) {
-          this.realizaConta()
           this.display.focus()
         }
       })
     },
 
-    btnParaDisplay: function (valor) {
-      this.display.value += valor
-    },
+    // Métodos:
 
-    clearDisplay: function () {
-      this.display.value = ''
+    //Adiciona um número no array de números que serão usados na operação
+    addNumbers: function (valor) {
+      parseFloat(valor)
+      this.numbers.push(valor)
+      this.display.value = 'Próximo valor:'
       this.display.focus()
     },
 
+    //Incrementa o display da calculadora
+    btnValueDisplay: function (valor) {
+      this.display.value += valor
+    },
+
+    //Limpa a calculadora
+    clear: function () {
+      this.numbers = []
+      this.amountFields = 0
+      this.display.value = `Digite a quantidade de campos envolvidos na operação:`
+      this.display.focus()
+    },
+
+    //Apaga o último digito do display
     deleteOne: function () {
       this.display.value = this.display.value.slice(0, -1)
     },
 
-    realizaConta: function () {
-      let conta = this.display.value
+    //Controla mensagens para o usuário
+    controllerDisplay: function () {
+      if (this.display.value === `Próximo valor:`) {
+        this.display.value = ''
+      }
 
-      try {
-        conta = eval(conta)
+      if (
+        this.display.value ===
+        `Digite a quantidade de campos envolvidos na operação:`
+      ) {
+        this.display.value = ''
+      }
 
-        if (!conta) {
-          alert('Conta inválida')
-        }
+      if (
+        this.display.value ===
+        'Por favor, preencha e confirme todos os campos reservados para a operação'
+      ) {
+        this.display.value = ''
+      }
 
-        this.display.value = conta
+      if (
+        this.display.value ===
+        `Por favor, digite os ${this.amountFields} números que serão utilizados na operação em sequência:`
+      ) {
+        this.display.value = ''
+      }
 
-        this.display.focus()
-      } catch (e) {
-        alert('Conta inválida')
-        return
+      if (
+        this.display.value === `Por favor, digite um número igual ao maior a 2`
+      ) {
+        this.display.value = ''
       }
     }
   }
